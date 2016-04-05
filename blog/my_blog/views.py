702fc3420dbb3bf461e834c40f8f5eef57f58cf5
Audrey Cole <audreycole88@yahoo.com>
2016-04-05 12:43:20 -0500

    Added posts on home page and post detail page

--- blog/my_blog/views.py
+++ blog/my_blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from django.utils import timezone
+
+from .models import Post # From current directory /models.py import Post model
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'my_blog/post_list.html', {})
+	# We want published blog posts sorted by published_date
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'my_blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    # post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'my_blog/post_detail.html', {'post': post})
