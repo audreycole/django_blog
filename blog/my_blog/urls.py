a2b1e86ece9c42022742e633a79613ed5e742468
Audrey Cole <audreycole88@yahoo.com>
2016-04-05 13:31:05 -0500

    now can add posts

--- blog/my_blog/urls.py
+++ blog/my_blog/urls.py
@@ -12,4 +12,5 @@ $ for end of text
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'), # Match localhost:8000/ to the view "post_list"
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/create/$', views.post_create, name='post_create'),
 ]
--- blog/my_blog/views.py
+++ blog/my_blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 
 from .models import Post # From current directory /models.py import Post model
 
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def post_list(request):
@@ -15,3 +17,14 @@ def post_detail(request, pk):
     # post = Post.objects.get(pk=pk)
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'my_blog/post_detail.html', {'post': post})
+
+def post_create(request):
+    title = request.POST.get('title')
+    text = request.POST.get('text')
+    author = User.objects.get(username='admin')
+
+    post = Post.objects.create(author=author, title=title, text=text)
+    post.publish()
+    
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'my_blog/post_list.html', {'posts': posts})
